9744b80ebdf033b4e84a0c81aa20642d6f15fe1c
kendall holland <holland.kd@gmail.com>
2023-03-28 09:12:18 -0400

    Ruby Basics Exercises

---
+++ exercise.rb
@@ -0,0 +1,24 @@
+# loop do
+#   puts 'Just keep printing...'
+#   break
+# end
+
+
+# loop do
+#   puts 'This is the outer loop.'
+#   loop do
+#     puts 'This is the inner loop.'
+#     break
+#   end
+#   break
+# end
+
+# puts 'This is outside all loops.'
+
+
+iterations = 1
+
+ while iterations < 6 do
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+end
